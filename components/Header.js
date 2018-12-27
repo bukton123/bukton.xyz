6370be8e45278ae08990146dd6e8508b1bc98be4
BukTon <buk_ton2@hotmail.com>
2018-12-27 17:03:30 +0700

    intro profile

--- components/Header.js
+++ components/Header.js
@@ -1,1 +1,75 @@
 import React from 'react'
+
+export default () => {
+
+  return (
+    <section className="header">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bukton.xyz">
+            <img src="/static/img/logo.png" width="30" />
+          </a>
+
+          {/* <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a> */}
+        </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">
+              Home
+            </a>
+            <a className="navbar-item" href="http://blog.bukton.xyz">
+              Blog
+            </a>
+          </div>
+
+          <div className="navbar-end">
+            <a
+              className='navbar-item is-hidden-touch is-hidden-desktop-only'
+              target='_blank'
+              href="https://github.com/bukton123">
+              <span className='icon'>
+                <i className="fab fa-github" />
+              </span>
+            </a>
+            <a
+              className='navbar-item is-hidden-touch is-hidden-desktop-only'
+              target='_blank'
+              href="https://www.linkedin.com/in/kanok-kanket-002391146/">
+              <span className='icon'>
+                <i className="fab fa-linkedin-in" />
+              </span>
+            </a>
+            <a
+              className='navbar-item is-hidden-touch is-hidden-desktop-only'
+              target='_blank'
+              href="https://medium.com/@bukton">
+              <span className='icon'>
+                <i class="fab fa-medium" />
+              </span>
+            </a>
+            <a
+              className='navbar-item is-hidden-touch is-hidden-desktop-only'
+              target='_blank'
+              href="https://www.facebook.com/buk.ton.3">
+              <span className='icon'>
+                <i class="fab fa-facebook" />
+              </span>
+            </a>
+            {/* <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Log in</strong>
+                </a>
+              </div>
+            </div> */}
+          </div>
+        </div>
+      </nav>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,11 @@ import Head from 'next/head'
 
 // import
 import MainScript from '../lib/main'
+import Header from '../components/Header'
+import Intro from '../components/Intro'
+
+// style
+import '../styles/styles.scss'
 
 class Bukton extends React.Component {
   static getInitialProps (ctx) {
@@ -20,7 +25,7 @@ class Bukton extends React.Component {
           <title>bukton.xyz</title>
 
           {/* <!-- Favicon--> */}
-          <link rel="icon" href="" type="image/png" />
+          <link rel="icon" href="/static/img/logo.png" type="image/png" />
           
           {/* <!-- Meta --> */}
           <meta charset="UTF-8" />
@@ -28,9 +33,13 @@ class Bukton extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet"/>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131386587-1"></script>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"/>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131386587-1" />
         </Head>
-        <div>Welcome to bukton.xyz!</div>
+        
+        {/* <!-- Content --> */}
+        <Header />
+        <Intro />
       </div>
     )
   }
