cbfe3e6c595fafc7d554bcae979cb128843aab16
BukTon <buk_ton2@hotmail.com>
2019-01-03 18:58:19 +0700

    pwa config webpack

--- next.config.js
+++ next.config.js
@@ -1,2 +1,22 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass()
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+
+module.exports = withSass({
+  webpack: (config) => {
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        verbose: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [
+          {
+            handler: 'networkFirst',
+            urlPattern: /^https?.*/
+          }
+        ]
+      })
+    )
+
+    return config
+  }
+})
+
--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,16 @@ class Bukton extends React.Component {
 
   componentDidMount () {
     MainScript()
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('service worker registration successful')
+        })
+        .catch(err => {
+          console.warn('service worker registration failed', err.message)
+        })
+    }
   }
 
   render () {
